b0b2c3575725cc431f8056d48b6bc5969d7d642e
Jerebtw <49494752+jerebtw@users.noreply.github.com>
2022-10-24 21:22:58 +0200

    Add docker-compose.yml

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapterNode from '@sveltejs/adapter-node';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -6,7 +6,7 @@ const config = {
 	preprocess: [preprocess()],
 
 	kit: {
-		adapter: adapter()
+		adapter: adapterNode()
 	}
 };
 
