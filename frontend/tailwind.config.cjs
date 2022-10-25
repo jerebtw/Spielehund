627af608b14fd1deeb6cc2a39c95a6ee18617967
C0dyN1k <drakekiller2004@gmail.com>
2022-10-25 12:20:04 +0200

    Add custome theme

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -2,8 +2,22 @@ const config = {
 	mode: "jit",
 	content: ["./src/**/*.{html,js,svelte,ts}"],
 
-	theme: {
-		extend: {}
+	daisyui: {
+		themes: [
+			{
+				mytheme: {
+					primary: "#5b21b6",
+					secondary: "#8b5cf6",
+					accent: "#2dd4bf",
+					neutral: "#191D24",
+					"base-100": "#2A303C",
+					info: "#06b6d4",
+					success: "#84cc16",
+					warning: "#f59e0b",
+					error: "#dc2626"
+				}
+			}
+		]
 	},
 
 	plugins: [require("daisyui")]
