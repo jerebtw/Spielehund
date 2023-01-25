4f5c0ba27e1da7d4b56db5d050dd26fc1f430ebe
Jerebtw <49494752+jerebtw@users.noreply.github.com>
2023-01-25 09:16:56 +0100

    HEHEHEHHEHEHEHHEEEEE

--- frontend/src/components/util/loading.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-	import { Spinner } from "flowbite-svelte";
-</script>
-
-<div class="absolute inset-0 flex justify-center items-center z-10">
-	<Spinner color="gray" />
-</div>
--- frontend/src/utils/types.ts
+++
@@ -1,5 +0,0 @@
-export interface GameData {
-	id: number;
-	name: string;
-	imageSrc: string;
-}
