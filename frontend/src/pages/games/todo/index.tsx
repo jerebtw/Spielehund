1aff0ba5d6704cf7450791417590e8c388fb5e7b
C0dyN1k <116630221+C0dyN1k@users.noreply.github.com>
2023-01-31 10:42:26 +0000

    Designed WiP Site

--- frontend/src/pages/games/todo/index.tsx
+++ frontend/src/pages/games/todo/index.tsx
@@ -1,3 +1,44 @@
+import {
+  Stack,
+  Container,
+  Card,
+  Group,
+  Title,
+  Image,
+  Text,
+  Button,
+} from "@mantine/core";
+import { useRouter } from "next/router";
+
 export default function TodoPage() {
-  return "WORK IN PROGRESS"
+  const router = useRouter();
+
+  return (
+    <Container py="5%">
+      <Card withBorder style={{ overflow: "scroll" }}>
+        <Stack>
+          <Group position="center">
+            <Title>Work in Progress</Title>
+            <Image
+              px={15}
+              py={15}
+              height="60%"
+              width="100%"
+              src="/WiP.jpg"
+              alt="With custom placeholder"
+              withPlaceholder
+              placeholder={
+                <Text align="center">
+                  Dieses Bild konnte leider nicht geladen werden
+                </Text>
+              }
+            />
+            <Button onClick={() => router.push("/")} color="violet">
+              Zurück zur Startseite
+            </Button>
+          </Group>
+        </Stack>
+      </Card>
+    </Container>
+  );
 }
