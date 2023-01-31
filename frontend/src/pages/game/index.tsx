8c79dc569bb9d80e7d3f1d599c9e8e3c538b14e3
Jerebtw <49494752+jerebtw@users.noreply.github.com>
2023-01-31 10:09:35 +0000

    Add work in progress page

--- frontend/src/pages/game/index.tsx
+++ frontend/src/pages/game/index.tsx
@@ -102,6 +102,7 @@ export default function GamePage() {
               )}
 
               <Button
+                loading={loading}
                 color="violet"
                 onClick={() => router.push(gameQuery.data.url)}>
                 Spielen
