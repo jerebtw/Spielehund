3e497e30084717b5dd54991c66d89ced21890012
Jerebtw <49494752+jerebtw@users.noreply.github.com>
2023-03-02 09:20:26 +0000

    Add padding

--- src/pages/games/jumpman/index.tsx
+++ src/pages/games/jumpman/index.tsx
@@ -162,13 +162,12 @@ export default function JumpManGame() {
               <Loading />
             ) : (
               <>
-                <Group position="apart" py="2%">
+                <Group position="apart" py="10%">
                   <Title order={3}>
                     Fehler: {`${currentImage}/${walkImages.length}`}
                   </Title>
                   <Title order={3}>
-                    {"Win/Lose: "}
-                    {(gameStatsQuery.data?.at(0)?.Rate || 0).toFixed(1)}
+                    {`Win/Lose: ${(gameStatsQuery.data?.at(0)?.Rate || 0).toFixed(1)}`}
                   </Title>
                 </Group>
 
